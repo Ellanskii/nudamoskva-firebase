586769291a4b7595bff33a93a83416e92dde9da2
Unknown <nameimprobable@gmail.com>
2018-09-27 16:08:17 +0300

    setting up environment

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    nav.navbar
+</template>
+<style lang="scss">
+    .navbar {
+        .navbar-brand {
+            height: 2rem;
+        }
+    }
+</style>
+
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app-wrapper">
     <app-header></app-header>
+    <nav-bar></nav-bar>
     <div id="content-wrapper" class="mui--text-center">
       <nuxt class="mui-container"/>
     </div>
@@ -9,23 +10,24 @@
 </template>
 
 <script>
-  import AppFooter from '@/components/Footer'
-  import AppHeader from '@/components/Header'
+import AppFooter from "@/components/Footer";
+import AppHeader from "@/components/Header";
+import NavBar from "@/components/Navbar";
 
-  export default {
-    components: { AppHeader, AppFooter },
-    methods: {
-      reloadPage () {
-        window.location.reload()
-      }
+export default {
+  components: { AppHeader, AppFooter, NavBar },
+  methods: {
+    reloadPage() {
+      window.location.reload();
     }
   }
+};
 </script>
 
 <style>
-  #app-wrapper {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-  }
+#app-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
 </style>
