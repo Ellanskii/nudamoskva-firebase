e4dd5cfed9e25bf56140a17fb6cb9f078e12f2e8
Elijah Ellanski <nameimprobable@gmail.com>
2018-10-04 11:14:32 +0300

    centeered editor

--- src/pages/editor.vue
+++ src/pages/editor.vue
@@ -1,30 +1,33 @@
 <template lang="pug">
-.hero 
+.hero.is-bold.is-warning
     .hero-body
         .container
-            .box: b-field(label="Название")
-                b-input(v-model="title")
-            .box: b-field(label="Тело истории")
-                vue-editor(v-model="content")
-            .box
-                b-field(label="Картиночки")
-                    b-upload(v-model="images" multiple drag-drop)
-                        .section.has-text-centered.content
-                            p: b-icon(icon="upload" size="is-large")
-                            p Нажмите или перетащите файлы для загрузки
-                .tags
-                    span(
-                        v-for="(file, index) in images"
-                        :key="index"
-                        class="tag is-primary"
-                        ) {{ file.name }}
-                        button.delete.is-small(
-                            @click="deleteImage(index)"
-                            type="button"
-                        )
+            .columns.is-centered
+                .column.is-10-tablet.is-8-desktop.is-6-fullhd
+                    h1.title Новая история
+                    .box: b-field(label="Название")
+                        b-input(v-model="title")
+                    .box: b-field(label="Тело истории")
+                        vue-editor(v-model="content")
+                    .box
+                        b-field(label="Картиночки")
+                            b-upload(v-model="images" multiple drag-drop)
+                                .section.has-text-centered.content
+                                    p: b-icon(icon="upload" size="is-large")
+                                    p Нажмите или перетащите файлы для загрузки
+                        .tags
+                            span(
+                                v-for="(file, index) in images"
+                                :key="index"
+                                class="tag is-primary"
+                                ) {{ file.name }}
+                                button.delete.is-small(
+                                    @click="deleteImage(index)"
+                                    type="button"
+                                )
 
-            .box
-                button.button(@click="upload(images[0])") Upload
+                    .box
+                        button.button(@click="upload(images[0])") Upload
 </template>
 
 <script>
